22b0ef3c84ccd409feb268cb7dc530133817b840
IH-M2 <ilhae.na@gmail.com>
2024-11-21 20:15:54 +0900

    새로운 할 일 추가 API 구현

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TASK (TITLE, DESCRIPTION, STATUS, DUE_DATE)
+    VALUES ('인텔리제이 강의 듣기', 'Ch01 끝내기', 'In_PROGRESS', now() + 1);
+
+INSERT INTO TASK (TITLE, DESCRIPTION, STATUS, DUE_DATE)
+    VALUES ('인텔리제이 강의 듣기', 'Ch02 끝내기', 'TODO', now() + 2);
